166b9b1d8580214ce50f90918d2ba2abe3227753
Georgios Rounis <georg.rounis@gmail.com>
2023-12-05 12:26:01 +0100

    implemented conditional rendering of header icons

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoChatbubbleEllipses } from "react-icons/io5";
 import {FiList} from "react-icons/fi";
 import { Tilt } from 'react-tilt';
@@ -7,11 +7,34 @@ import AddTask from "./AddTask";
 import UserHeaderInfo from "./UserHeaderInfo";
 import { RootState } from "../Redux/store";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const logo = require ("../Assets/logo192.png");
 
 const Header = () => {
     const currentUser = useSelector((state:RootState) => state.user.currentUser);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const goTo = useNavigate();
+
+    const handleSignOut = () => {
+        
+    }
+
+    const toggleDropdown = () => {
+      setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleGoToPage = (page:string) => {
+        goTo(page)
+        setCurrentPage(page)
+    }
+
+    const setCurrentPage = (page:string) => {
+        localStorage.setItem("user-page", page);
+    }
+
+    const getCurrentPage = () => {
+        return localStorage.getItem("user-page");
+    }
 
     return (
         <div className="flex flex-wrap gap-5 justify-between items-center sm:flex-row bg-gradient-to-r from-black to-black px-5 py-5">
@@ -19,17 +42,36 @@ const Header = () => {
                 <img  src={logo} alt='logo' className="cursor-pointer" />
             </Tilt>
             <div className="flex flex-row-reverse md:flex-row items-center justify-center gap-5 flex-wrap ">
+                {(getCurrentPage() === '/dashboard')
+                ?
+                <>
                 <AddTask />
-                <Icon  IconName={IoChatbubbleEllipses} ping = {true} className="block"/>
-                <Icon  IconName={FiList} className="block"/>
-                <div className="group relative">
+                <Icon  IconName={IoChatbubbleEllipses} ping = {true} className="block" onClick={() => handleGoToPage('/dashboard/chat')}/>
+                </>
+                : (getCurrentPage() === '/dashboard/chat')
+                ?
+                <>
+                <Icon  IconName={FiList} className="block" onClick={() => handleGoToPage('/dashboard')}/>
+                </>
+                :
+                <>
+                <Icon  IconName={FiList} className="block" onClick={() => handleGoToPage('/dashboard')}/>
+                <Icon  IconName={IoChatbubbleEllipses} ping = {true} className="block" onClick={() => handleGoToPage('/dashboard/chat')}/>
+                </>
+                }
+
+                {/* <AddTask />
+                <Icon  IconName={IoChatbubbleEllipses} ping = {true} className="block" onClick={() => handleGoToPage('/dashboard/chat')}/>
+                <Icon  IconName={FiList} className="block" onClick={() => handleGoToPage('/dashboard/list')}/> */}
+
+                <div className="group relative" onClick={toggleDropdown} >
                     <UserHeaderInfo user = {currentUser} />
-                    <div className="absolute pt-5 hidden group-hover:block w-full min-w-max">
+                    <div className={` pt-5 absolute ${isDropdownOpen ? 'block' : 'hidden'} w-full min-w-max`}>
                         <ul className="w-full bg-white overflow-hidden rounded-md shadow-md text-gray-700 pt-1">
-                            <Link to= "/dashboard/profile" className="hover: bg-gray-200 py-2 block">
+                            <Link to= "/dashboard/profile" className="cursor-pointer hover: bg-gray-200 py-2 block" onClick= {() => handleGoToPage('/dashboard/profile')}>
                                 Profile
                             </Link>
-                            <Link to= "/login" className="hover: bg-gray-200 py-2 block">
+                            <Link to= "/login" className=" cursor-pointer hover: bg-gray-200 py-2 block" onClick= {() => handleSignOut}>
                                 Logout
                             </Link>
                         </ul>
--- src/Pages/ListPage.tsx
+++ src/Pages/ListPage.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const ListPage = () => {
-    const storedUserData = JSON.parse(localStorage.getItem('user') || '{}');
-    const bio = storedUserData.bio ;
+
     return (
-        <div className="text-black text-center opacity-80 font-bold text-4xl md:text-6xl mb-10">
-            <h1 >{bio}</h1>
+        <div>
+            <h1 >Listpage</h1>
         </div>
     )
 }
