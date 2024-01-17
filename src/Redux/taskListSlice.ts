0a2b3e56896736a41a3392ae69d268a909ab4f7a
Georgios Rounis <georg.rounis@gmail.com>
2024-01-17 12:13:25 +0100

    implemented taskListSlice for redux state management

---
+++ src/Redux/taskListSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { taskListType, taskType } from "../Types";
+
+export const defaultTaskList: taskListType = {
+    title:"Sample Task List"
+}
+
+export const defaultTask: taskType = {
+    title:"Task title",
+    description: "task description"
+}
+
+type taskListSliceType = {
+    currentTaskList:[],
+}
+
+const initialState = {
+    currentTaskList:[]
+}
+
+const taskListSlice = createSlice({
+    name:"taskList",
+    initialState,
+    reducers:{
+        setTaskList:(state,action) => {
+
+        },
+        addTaskList:(state,action) => {
+            
+        }
+    }
+});
+
+export const { setTaskList, addTaskList } = taskListSlice.actions;
+export default taskListSlice.reducer;
